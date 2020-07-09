47810694deac035e20ea458fd611a7817cc318b4
Jeroen Manders <jeroen@manders.be>
2020-07-09 08:37:23 +0000

    Make Route53 record optional

--- packets/Terraform Route53 A Alias record/files/route53_a_alias_record.tf
+++ packets/Terraform Route53 A Alias record/files/route53_a_alias_record.tf
@@ -1,6 +1,11 @@
-resource "aws_route53_record" "$instance.getAttribute("route53_name")" {
-  zone_id = $instance.getAttribute("route53_zone_id")
-  name    = "$instance.getAttribute("route53_name")"
+#set ($route53Name = $instance.getAttribute("route53_name"))
+#set ($zoneId = $instance.getAttribute("route53_zone_id"))
+#if ($zoneId == "")
+#stop
+#end
+resource "aws_route53_record" "$route53Name" {
+  zone_id = $zoneId
+  name    = "$route53Name"
   type    = "A"
   alias {
     name                   = $instance.getAttribute("target_name")
